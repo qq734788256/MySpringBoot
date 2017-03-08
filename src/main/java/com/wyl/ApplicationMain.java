ff4f9a588af0f40e1780c99b1cb7faed1a472792
wangyunlong <734788256@qq.com>
2017-03-08 17:35:16 +0800

    增加注释

--- src/main/java/com/wyl/ApplicationMain.java
+++ src/main/java/com/wyl/ApplicationMain.java
@@ -8,7 +8,8 @@ import org.springframework.context.annotation.Configuration;
 //@SpringBootApplication //可代替下面的三个
 @Configuration//配置控制  
 @EnableAutoConfiguration//启用自动配置   Spring Boot建议只有一个带有该注解的类
-@ComponentScan//组件扫描  
+@ComponentScan(basePackages = {"com.wyl.a"})//组件扫描
+//@ComponentScan 如果不写basePackages的话默认扫描该文件所在路径下的所有内容
 public class ApplicationMain {
     public static void main(String[] args) throws Exception {
         //启动Spring Boot项目的唯一入口  
